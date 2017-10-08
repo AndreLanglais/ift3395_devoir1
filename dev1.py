c635ad77e8013738eb41af4a467dcb793c26aa20
Andre <derkan.man@gmail.com>
2017-10-08 15:54:19 -0400

    classifieur bayes question jusqua 3

--- dev1.py
+++ dev1.py
@@ -157,6 +157,177 @@ utilitaires.gridplot(classifieur,
                          iris_test[:, train_cols + [-1]],
                          n_points=50)
 
+print "Bayes Diagonal 2D"
 print "Taux d'erreur (entrainement) %.2f%%" % ((1-(classesPred_train==iris_train[:,-1]).mean())*100.0)
 print "Taux d'erreur (test) %.2f%%" % ((1-(classesPred_test==iris_test[:,-1]).mean())*100.0)
 
+#Bayes 4D
+
+train_cols = [0,3]
+
+model_classe1_diag = gauss_diag(len(train_cols))
+model_classe2_diag = gauss_diag(len(train_cols))
+model_classe3_diag = gauss_diag(len(train_cols))
+
+model_classe1_diag.train(iris_train1[:,train_cols])
+model_classe2_diag.train(iris_train2[:,train_cols])
+model_classe3_diag.train(iris_train3[:,train_cols])
+
+#liste modeles et priors
+models_diag = [model_classe1_diag,model_classe2_diag,model_classe3_diag]
+priors = [0.3333, 0.3333, 0.3333]
+
+#classifieur
+classifieur = classif_bayes(models_diag,priors)
+
+#calcul des probs
+log_prob_train=classifieur.compute_predictions(iris_train[:, train_cols])
+log_prob_test=classifieur.compute_predictions(iris_test[:, train_cols])
+
+#
+classesPred_train = log_prob_train.argmax(1)+1
+classesPred_test = log_prob_test.argmax(1)+1
+
+print "Bayes Diagnoal 4D"
+print "Taux d'erreur (entrainement) %.2f%%" % ((1-(classesPred_train==iris_train[:,-1]).mean())*100.0)
+print "Taux d'erreur (test) %.2f%%" % ((1-(classesPred_test==iris_test[:,-1]).mean())*100.0)
+
+
+# Bayes Parzen 2d
+
+#parzen sigma petit
+train_cols = [0,1]
+sigma_petit = 1
+sigma_grand = 10
+sigma_app = 2
+
+model_classe1_parzen = parzen(len(train_cols),sigma_petit)
+model_classe2_parzen = parzen(len(train_cols),sigma_petit)
+model_classe3_parzen = parzen(len(train_cols),sigma_petit)
+
+model_classe1_parzen.train(iris_train1[:,train_cols])
+model_classe2_parzen.train(iris_train2[:,train_cols])
+model_classe3_parzen.train(iris_train3[:,train_cols])
+
+#liste modeles et priors
+models_parzen = [model_classe1_parzen,model_classe2_parzen,model_classe3_parzen]
+priors = [0.3333, 0.3333, 0.3333]
+
+#classifieur
+classifieur = classif_bayes(models_parzen,priors)
+
+#calcul des probs
+log_prob_train=classifieur.compute_predictions(iris_train[:, train_cols])
+log_prob_test=classifieur.compute_predictions(iris_test[:, train_cols])
+
+#
+classesPred_train = log_prob_train.argmax(1)+1
+classesPred_test = log_prob_test.argmax(1)+1
+
+utilitaires.gridplot(classifieur,
+                         iris_train[:, train_cols + [-1]],
+                         iris_test[:, train_cols + [-1]],
+                         n_points=50)
+
+print "Parzen sigma petit"
+print "Taux d'erreur (entrainement) %.2f%%" % ((1-(classesPred_train==iris_train[:,-1]).mean())*100.0)
+print "Taux d'erreur (test) %.2f%%" % ((1-(classesPred_test==iris_test[:,-1]).mean())*100.0)
+
+#parzen sigma grand
+model_classe1_parzen = parzen(len(train_cols),sigma_grand)
+model_classe2_parzen = parzen(len(train_cols),sigma_grand)
+model_classe3_parzen = parzen(len(train_cols),sigma_grand)
+
+model_classe1_parzen.train(iris_train1[:,train_cols])
+model_classe2_parzen.train(iris_train2[:,train_cols])
+model_classe3_parzen.train(iris_train3[:,train_cols])
+
+#liste modeles et priors
+models_diag = [model_classe1_parzen,model_classe2_parzen,model_classe3_parzen]
+priors = [0.3333, 0.3333, 0.3333]
+
+#classifieur
+classifieur = classif_bayes(models_diag,priors)
+
+#calcul des probs
+log_prob_train=classifieur.compute_predictions(iris_train[:, train_cols])
+log_prob_test=classifieur.compute_predictions(iris_test[:, train_cols])
+
+#
+classesPred_train = log_prob_train.argmax(1)+1
+classesPred_test = log_prob_test.argmax(1)+1
+
+utilitaires.gridplot(classifieur,
+                         iris_train[:, train_cols + [-1]],
+                         iris_test[:, train_cols + [-1]],
+                         n_points=50)
+
+print "Parzen Sigma Grand"
+print "Taux d'erreur (entrainement) %.2f%%" % ((1-(classesPred_train==iris_train[:,-1]).mean())*100.0)
+print "Taux d'erreur (test) %.2f%%" % ((1-(classesPred_test==iris_test[:,-1]).mean())*100.0)
+
+#parzen sigma approprie
+model_classe1_parzen = parzen(len(train_cols),sigma_app)
+model_classe2_parzen = parzen(len(train_cols),sigma_app)
+model_classe3_parzen = parzen(len(train_cols),sigma_app)
+
+model_classe1_parzen.train(iris_train1[:,train_cols])
+model_classe2_parzen.train(iris_train2[:,train_cols])
+model_classe3_parzen.train(iris_train3[:,train_cols])
+
+#liste modeles et priors
+models_diag = [model_classe1_parzen,model_classe2_parzen,model_classe3_parzen]
+priors = [0.3333, 0.3333, 0.3333]
+
+#classifieur
+classifieur = classif_bayes(models_diag,priors)
+
+#calcul des probs
+log_prob_train=classifieur.compute_predictions(iris_train[:, train_cols])
+log_prob_test=classifieur.compute_predictions(iris_test[:, train_cols])
+
+#
+classesPred_train = log_prob_train.argmax(1)+1
+classesPred_test = log_prob_test.argmax(1)+1
+
+utilitaires.gridplot(classifieur,
+                         iris_train[:, train_cols + [-1]],
+                         iris_test[:, train_cols + [-1]],
+                         n_points=50)
+
+print "Parzen sigma app"
+print "Taux d'erreur (entrainement) %.2f%%" % ((1-(classesPred_train==iris_train[:,-1]).mean())*100.0)
+print "Taux d'erreur (test) %.2f%%" % ((1-(classesPred_test==iris_test[:,-1]).mean())*100.0)
+
+
+#Courbes apprentissages
+
+sigmas = np.linspace(1,5,100)
+
+for i,k in enumerate(sigmas):
+    model_classe1_parzen = parzen(len(train_cols), k)
+    model_classe2_parzen = parzen(len(train_cols), k)
+    model_classe3_parzen = parzen(len(train_cols), k)
+
+    model_classe1_parzen.train(iris_train1[:, train_cols])
+    model_classe2_parzen.train(iris_train2[:, train_cols])
+    model_classe3_parzen.train(iris_train3[:, train_cols])
+
+    # liste modeles et priors
+    models_diag = [model_classe1_parzen, model_classe2_parzen, model_classe3_parzen]
+    priors = [0.3333, 0.3333, 0.3333]
+
+    # classifieur
+    classifieur = classif_bayes(models_diag, priors)
+
+    # calcul des probs
+    log_prob_train = classifieur.compute_predictions(iris_train[:, train_cols])
+    log_prob_test = classifieur.compute_predictions(iris_test[:, train_cols])
+
+    #
+    classesPred_train = log_prob_train.argmax(1) + 1
+    classesPred_test = log_prob_test.argmax(1) + 1
+
+    print "Parzen sigma %.2f%%" % k
+    print "Taux d'erreur (entrainement) %.2f%%" % ((1 - (classesPred_train == iris_train[:, -1]).mean()) * 100.0)
+    print "Taux d'erreur (test) %.2f%%" % ((1 - (classesPred_test == iris_test[:, -1]).mean()) * 100.0)
