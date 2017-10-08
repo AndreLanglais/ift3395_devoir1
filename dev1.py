4935335be51943971734aecdfe412c595ba9b855
Andre <derkan.man@gmail.com>
2017-10-08 15:04:16 -0400

    classifieur bayes

--- dev1.py
+++ dev1.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pylab
+import utilitaires
 
 class gauss_diag:
     def __init__(self, n_dims):
@@ -48,6 +49,32 @@ class parzen:
 
         return log_prob
 
+
+class classif_bayes:
+
+    def __init__(self, modeles_diag, priors):
+        self.modeles_diag = modeles_diag
+        self.priors = priors
+        if len(self.modeles_diag) != len(self.priors):
+            print 'Le nombre de modeles MV doit etre egale au nombre de priors!'
+
+        self.n_classes = len(self.modeles_diag)
+
+    # Retourne une matrice de taille nb. ex. de test x nombre de classes contenant les log
+    # probabilités de chaque exemple de test sous chaque modèle MV.
+    def compute_predictions(self, test_data, eval_by_group=False):
+        log_pred = np.empty((test_data.shape[0], self.n_classes))
+
+        for i in range(self.n_classes):
+            # ici il va falloir utiliser modeles_mv[i] et priors pour remplir
+            # chaque colonne de log_pred (c'est plus efficace de faire tout une
+            # colonne a la fois)
+
+            log_pred[:, i] = self.modeles_diag[i].compute_predictions(test_data) + np.log(self.priors[i])
+
+        return log_pred
+
+
 # On decoupe les donnees en train/test
 iris=np.loadtxt('iris.txt')
 np.random.seed(123)
@@ -66,6 +93,8 @@ iris_test2 = iris[indices2[35:]]
 iris_train3 = iris[indices3[:35]]
 iris_test3 = iris[indices3[35:]]
 
+iris_train = np.concatenate([iris_train1, iris_train2, iris_train3])
+iris_test = np.concatenate([iris_test1, iris_test2, iris_test3])
 
 model = gauss_diag(4)
 model.train(iris_train1)
@@ -85,8 +114,8 @@ pylab.plot(iris_train1[:, 0], len(iris_train1) * [0], "o")
 
 parzen1 = parzen(4, 2)
 parzen1.train(iris_train1)
-prob = np.exp(parzen1.compute_predictions(iris_test1))
-parzen_graph = iris_test1[:, 0]
+prob = np.exp(parzen1.compute_predictions(iris_train1[0:,-1]))
+parzen_graph = iris_train1[:, 0]
 parzen_graph.sort()
 pylab.plot(parzen_graph, prob, color="blue")
 
@@ -95,4 +124,39 @@ pylab.plot(s, 1/(sigma * np.sqrt(2 * np.pi)) *
            np.exp(- (s - mu)**2 / (2 * sigma**2)), color="red")
 
 pylab.show()
+
+#Bayes Diagonal 2d
+
+train_cols = [0,1]
+
+model_classe1_diag = gauss_diag(len(train_cols))
+model_classe2_diag = gauss_diag(len(train_cols))
+model_classe3_diag = gauss_diag(len(train_cols))
+
+model_classe1_diag.train(iris_train1[:,train_cols])
+model_classe2_diag.train(iris_train2[:,train_cols])
+model_classe3_diag.train(iris_train3[:,train_cols])
+
+#liste modeles et priors
+models_diag = [model_classe1_diag,model_classe2_diag,model_classe3_diag]
+priors = [0.3333, 0.3333, 0.3333]
+
+#classifieur
+classifieur = classif_bayes(models_diag,priors)
+
+#calcul des probs
+log_prob_train=classifieur.compute_predictions(iris_train[:, train_cols])
+log_prob_test=classifieur.compute_predictions(iris_test[:, train_cols])
+
+#
+classesPred_train = log_prob_train.argmax(1)+1
+classesPred_test = log_prob_test.argmax(1)+1
+
+utilitaires.gridplot(classifieur,
+                         iris_train[:, train_cols + [-1]],
+                         iris_test[:, train_cols + [-1]],
+                         n_points=50)
+
+print "Taux d'erreur (entrainement) %.2f%%" % ((1-(classesPred_train==iris_train[:,-1]).mean())*100.0)
+print "Taux d'erreur (test) %.2f%%" % ((1-(classesPred_test==iris_test[:,-1]).mean())*100.0)
 
